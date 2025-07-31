3a386a1f0c56ba11c9bc32fa89a7c0c0fdabf197
Ahmet <66086492+Wiskiee@users.noreply.github.com>
2025-07-31 15:19:33 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,8 +4,14 @@ var headerImg = document.getElementById("headerImage")
 var desc1= document.getElementById("descPart1")
 var desc2= document.getElementById("descPart2")
 
-setInterval(icon1Sleep,1100);
-setInterval(icon4Sleep,1100);
+if(window.innerWidth>768){
+    setInterval(icon1Sleep,1100);
+    setInterval(icon4Sleep,1100);
+}else{
+    setInterval(icon1Sleep2,1100)
+    setInterval(icon4Sleep2,1100)
+}
+
 setInterval(sleepImage,2200)
 setInterval(sleepDesc,3000)
 
@@ -24,4 +30,12 @@ function icon4Sleep(){
 }
 function sleepImage(){
     headerImg.style.opacity="100%"
+}
+function icon1Sleep2(){
+    icon1.style.left="13%"
+    icon1.style.color="#fff"
+}
+function icon4Sleep2(){
+    icon3.style.right="13%"
+    icon3.style.color="#fff"
 }
